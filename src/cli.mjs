5f613cbddcef939a19b89d4735c5e63132a0239b
Vetési Zoltán <vetesi.zoltan@gmail.com>
2018-07-25 10:06:16 +0200

    wip: rewrite

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,4 +1,15 @@
+import path from "path"
+import url from "url"
 import { cli } from "@anzar/build"
 cli.init(process.argv)
 
-import "webpack-cli/bin/webpack"
+const wp = new cli.Webpack()
+const cfg = url.format({
+    pathname: path.join(process.cwd(), "webpack.config.mjs"),
+    protocol: "file:",
+    slashes: true
+})
+console.log(cfg)
+wp.loadConfig(cfg)
+
+// import "webpack-cli/bin/webpack"
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -14,37 +14,42 @@ options.setAll({
 
 
 export default config("@anzar/build", {
+    whenMode: {
+        development(cfg, key) {
+            return {
+                devServer: {
+                    contentBase: path.join(options.package_path, "dist", "[__MODE__]"),
+                    port: 4200,
+                    hot: options.hot,
+                    historyApiFallback: true,
+                    clientLogLevel: "error",
+                    stats: "errors-only"
+                },
 
-}).ifMode("development", {
-    devServer: {
-        contentBase: path.join(options.package_path, "dist", "[__MODE__]"),
-        port: 4200,
-        hot: options.hot,
-        historyApiFallback: true,
-        clientLogLevel: "error",
-        stats: "errors-only"
-    },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin({
-            chunksSortMode: "dependency",
-            inject: false,
-            template: path.join(__dirname, "index.pug")
-        })
-    ]
-}).define((defs, cfg, key) => {
-    defs.set("__DEV_SERVER__", () => {
-        if (options.isServing && cfg.devServer) {
-            let dvs = cfg.devServer
-            return url.format({
-                protocol: dvs.https ? "https" : "http",
-                hostname: dvs.host ? dvs.host : "localhost",
-                port: dvs.port
-            })
-        } else {
-            return null
+                plugins: [
+                    new webpack.HotModuleReplacementPlugin(),
+                    new webpack.NamedModulesPlugin(),
+                    new HtmlWebpackPlugin({
+                        chunksSortMode: "dependency",
+                        inject: false,
+                        template: path.join(__dirname, "index.pug")
+                    })
+                ]
+            }
         }
-    })
+    },
+    constants: {
+        __DEV_SERVER__(defs, cfg, key) {
+            if (options.isServing && cfg.devServer) {
+                let dvs = cfg.devServer
+                return url.format({
+                    protocol: dvs.https ? "https" : "http",
+                    hostname: dvs.host ? dvs.host : "localhost",
+                    port: dvs.port
+                })
+            } else {
+                return null
+            }
+        }
+    }
 })
