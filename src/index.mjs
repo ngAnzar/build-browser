86878a0ac38ac03a369fe053c36a7ad3f1087428
Vetési Zoltán <zozzz@trigon.hu>
2019-02-01 14:36:17 +0100

    feat: relative assets url

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,5 @@
+import path from "path"
+
 import HtmlWebpackPlugin from "html-webpack-plugin"
 import { options } from "@anzar/build"
 import nzStyle from "@anzar/build/src/plugins/style"
@@ -18,24 +20,45 @@ export function htmlPlugin(tpl, pluginOptions) {
 
             for (let entry of data.css) {
                 let norm = entry.replace(/\\+/, "/")
-                css[norm] = { path: norm, media: "all" }
+
+                css[norm] = {
+                    path: relativizePath(options.relative_assets, norm),
+                    media: "all"
+                }
             }
 
             for (let filePath in compilation.assets) {
                 let entry = compilation.assets[filePath]
                 if (entry instanceof nzStyle.CssSource) {
                     filePath = filePath.replace(/\\+/, "/")
-                    css[filePath] = { path: `/${filePath}`, media: entry.media }
+                    css[filePath] = {
+                        path: relativizePath(options.relative_assets, `/${filePath}`),
+                        media: entry.media
+                    }
                 }
             }
 
             return {
                 title: options.TITLE,
-                js: data.js.map((entry) => { return { path: entry } }),
+                js: data.js.map((entry) => { return { path: relativizePath(options.relative_assets, entry) } }),
                 css: Object.values(css),
                 ...params
             }
         },
         ...pluginOptions
     })
+}
+
+
+function relativizePath(relFrom, assetPath) {
+    if (relFrom) {
+        let assetAbsPath = path.join(options.out_path, assetPath.replace(/^[\\\/]+/, ""))
+        let relFromAbs = relFrom === true
+            ? options.out_path
+            : path.isAbsolute(relFrom)
+                ? relFrom
+                : path.join(options.out_path, relFrom)
+        return path.relative(relFromAbs, assetAbsPath).replace(/\\+/, "/")
+    }
+    return assetPath
 }
